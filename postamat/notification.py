ca3e11222972021f6930430d21bc4d2466f908f2
Vitovt <59355907+Vitek14@users.noreply.github.com>
2026-07-08 13:41:13 +0300

    Revert "Badges"

--- postamat/notification.py
+++
@@ -1,6 +0,0 @@
-from .interfaces import UserNotificationApi
-
-
-class NotificationClient(UserNotificationApi):
-    def send_receive_code(self, phone: str, receive_code: str, order_id: str) -> None:
-        print(f"SMS to {phone}: code {receive_code} for order {order_id}")
