4dbe34039deac3b180c782e051fb47a080ca4d32
Vitek14 <yudin.vitovt@gmail.com>
2026-07-09 10:48:22 +0300

    updated gitignore and settings file

--- postamat_project/settings.py
+++ postamat_project/settings.py
@@ -13,6 +13,7 @@ import dj_database_url
 import os
 from pathlib import Path
 
+from django.conf.global_settings import DATABASES
 from django.contrib import staticfiles
 from dotenv import load_dotenv
 
@@ -126,13 +127,11 @@ WSGI_APPLICATION = 'postamat_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+DATABASES['default'] = dj_database_url.config(
+    default=os.environ.get('DATABASE_URL'),
+    conn_max_age=600,
+    ssl_require=True
+)
 
 
 # Password validation
