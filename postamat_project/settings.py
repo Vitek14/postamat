1f2401e9e36f61bc2c73fd9d04edaba23357a226
Vitek14 <yudin.vitovt@gmail.com>
2026-07-09 11:26:07 +0300

    removed unused parameters from ci and added debug log

--- postamat_project/settings.py
+++ postamat_project/settings.py
@@ -19,6 +19,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print("RAW DATABASE_URL:", os.environ.get('DATABASE_URL'))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
